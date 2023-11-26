23ecfee173577bdf8b4a60b5a9c42591715f1151
Irshath <irshath700@gmail.com>
2023-11-26 18:10:39 +0530

    add shadcn-ui button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,63 +3,32 @@ import Link from "next/link";
 import { CreatePost } from "@/app/_components/create-post";
 import { useSession } from "next-auth/react";
 import { api } from "@/trpc/react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const { data: hello } = api.post.hello.useQuery({ text: "Hello from TRPC" });
   const { data: session } = useSession();
-  const { data, isLoading } = api.post.sayHi.useQuery();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <h5>{isLoading ? "Loading" : data}</h5>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
+    <main className="container">
+      <h1 className="my-10 text-center text-3xl font-bold tracking-tighter">
+        NextJS + NextAuth + DrizzleORM + PlanetScale + shadcn-ui
+      </h1>
+      <p className="text-center text-2xl">
+        {hello ? hello.greeting : "Loading tRPC query..."}
+      </p>
+      <div className="flex w-full flex-col items-center justify-center gap-4">
+        <p className="text-center">
+          {session && <span>Logged in as {session.user?.name}</span>}
+        </p>
+        <Button asChild variant="link">
+          <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+            {session ? "Sign out" : "Sign in"}
           </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
-          </div>
-        </div>
-
-        <CrudShowcase />
+        </Button>
       </div>
+
+      <CrudShowcase />
     </main>
   );
 }
